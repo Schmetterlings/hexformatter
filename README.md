7ed969cff519deffd22ebe5092e402a5be6c2e0f
Kamil Marut <kakkmar@gmail.com>
2019-04-09 17:39:37 +0200

    Formatting of bits containing frame informations

--- README.md
+++ README.md
@@ -5,16 +5,13 @@ Uses files with proprietary *.spc* extension, strongly hardcoded.
 
 ## Order of data in bytes
 
-1. Time [h]
-2. Time [m]
-3. Time [s]
-4. Time [ms]
-5. Length, Frame type, ID type
+1-4: Time [ms]  
+5: Length, Frame type, ID type
   - Bit nr 0 -> ID Type
   - Bit nr 1 -> Frame Type
   - Bits nr 2, 3 -> Empty, reserved for future use
   - Bits nr 4, 5, 6, 7 -> Length of data
-  
-6. 1st part of frame ID (only bits nr 5, 6, 7 are used, the rest are empty)
-7. 2nd part of frame ID (all bits used)
-8. Data
+
+6: 1st part of frame ID (only bits nr 5, 6, 7 are used, the rest are empty)  
+7: 2nd part of frame ID (all bits used)  
+8: Data
--- hexformatter.py
+++ hexformatter.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import sys
 
 
 def format_file(filename):
@@ -15,7 +16,7 @@ def format_file(filename):
         hex_list = ['{:02X}'.format(c) for c in file.read()]
 
     formatted_list = []
-    last_id = 2  # Omit the header bits
+    last_id = 2  # Omit the header bytes
 
     for idx, _ in enumerate(hex_list):
         if hex_list[idx] == "CC":
@@ -27,9 +28,57 @@ def format_file(filename):
     return formatted_list
 
 
+def format_frame_info(hex):
+    """
+    Converts hex to binary and gets data from bits.
+
+    :param hex: 2 character string
+    :return: string
+    """
+    info = ""
+    binary = bin(int(hex, 16))[2:].zfill(8)
+
+    frame_length = int(binary[:4], 2)
+    info += "Frame length: {}\n".format(frame_length)
+
+    frame_type = int(binary[6:7])
+    if frame_type == 0:
+        info += "Frame type: RTR\n"
+    else:
+        info += "Frame type: DATA\n"
+
+    id_type = int(binary[7:8])
+    if id_type == 0:
+        info += "ID type: AVR\n"
+    else:
+        info += "ID type: STD\n"
+
+    return info
+
+
+def format_frame_id(hex):
+    """
+    Converts hex to binary and gets frame id.
+
+    :param hex: 4 character string
+    :return: string
+    """
+    binary = (bin(int(hex, 16))[2:].zfill(16))[:11]
+
+    return "Frame ID: {}".format(int(binary, 2))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Format and display information from raw SD card file.")
     parser.add_argument("file", help="Path to raw SD card file.")
     args = parser.parse_args()
 
-    print(format_file(args.file))
+    frame_list = format_file(args.file)
+
+    for frame in frame_list:
+        frame_arrival_time = int(frame[3] + frame[2] + frame[1] + frame[0], 16)
+
+        print(format_frame_info(frame[4]))
+        print(format_frame_id(frame[6] + frame[5]))
+
+        print("Frame arrival: {} [ms]".format(frame_arrival_time))
